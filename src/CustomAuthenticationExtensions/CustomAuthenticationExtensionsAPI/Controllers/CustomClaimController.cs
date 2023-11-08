d05b6c1708bbc27e890c489cb3eb1f05e883a803
Jasper Hedegaard Bojsen <jasperhb@outlook.com>
2023-11-08 22:46:59 +0100

    refactor

--- src/CustomAuthenticationExtensions/CustomAuthenticationExtensionsAPI/Controllers/CustomClaimController.cs
+++ src/CustomAuthenticationExtensions/CustomAuthenticationExtensionsAPI/Controllers/CustomClaimController.cs
@@ -12,18 +12,18 @@ public class CustomClaimController : Controller
     [HttpPost]
     public IActionResult Post(CustomerExtensionRequest request)
     {
-        // Read the correlation ID from the Azure AD  request    
+        // Read the correlation ID from the Azure AD request    
         string? correlationId = request?.Data?.AuthenticationContext?.CorrelationId;
 
         // Claims to return to Azure AD
-        CustomerExtensionResponse responseContent = new();
+        CustomerExtensionResponse response = new();
 
-        responseContent.Data.Actions[0].Claims.CorrelationId = correlationId;
-        responseContent.Data.Actions[0].Claims.ApiVersion = "1.0.0";
-        responseContent.Data.Actions[0].Claims.DateOfBirth = "01/01/2000";
-        responseContent.Data.Actions[0].Claims.CustomRoles.Add("Writer");
-        responseContent.Data.Actions[0].Claims.CustomRoles.Add("Editor");
+        response.Data.Actions[0].Claims.CorrelationId = correlationId;
+        response.Data.Actions[0].Claims.ApiVersion = "1.0.0";
+        response.Data.Actions[0].Claims.DateOfBirth = "01/01/2000";
+        response.Data.Actions[0].Claims.CustomRoles.Add("Writer");
+        response.Data.Actions[0].Claims.CustomRoles.Add("Editor");
 
-        return new OkObjectResult(responseContent);
+        return new OkObjectResult(response);
     }
 }
